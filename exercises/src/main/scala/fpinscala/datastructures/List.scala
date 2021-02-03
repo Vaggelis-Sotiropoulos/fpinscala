ccf4706d6c507ef1e6c694f94e05e5dec695b2a8
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-02-03 11:00:56 -0800

    FPSCALA-CHAPTER3: Edits reverse implementation

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -104,7 +104,7 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldLeft(l, 0)((acc, _) => acc + 1)
 
   def reverse[A](l: List[A]): List[A] =
-    foldLeft(l, List())((newL, h) => append(newL, reverse(tail(l))))
+    foldLeft(l, List())((newL, h) => Cons(h, newL))
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
