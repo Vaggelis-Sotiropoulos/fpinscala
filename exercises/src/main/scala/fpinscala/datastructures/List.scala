d8b25bffbc3c105d32c82f5625ae0f88c98233d0
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-02-03 10:51:59 -0800

    FPSCALA-CHAPTER3: List reverse implementation attempt

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -103,5 +103,8 @@ object List { // `List` companion object. Contains functions for creating and wo
   def length2[A](l: List[A]): Int =
     foldLeft(l, 0)((acc, _) => acc + 1)
 
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, List())((newL, h) => append(newL, reverse(tail(l))))
+
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
