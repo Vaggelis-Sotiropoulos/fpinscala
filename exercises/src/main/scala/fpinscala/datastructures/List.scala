fe916d4314a43b51d056981599d54719bc9d240d
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-02-02 20:46:53 -0800

    FPSCALA-CHAPTER3: Edits dropWhile function

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -71,8 +71,8 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
     l match {
-      case Nil => Nil
-      case Cons(h, t) => if(f(h)) dropWhile(drop(l, 1), f) else dropWhile(t, f)
+      case Cons(h,t) if f(h) => dropWhile(t, f)
+      case _ => l
     }
 
   def init[A](l: List[A]): List[A] = ???
