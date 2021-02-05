55db558775d1d7ed9427fa18058e234ddd6ef82b
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-02-05 13:26:28 -0800

    FPSCALA-CHAPTER3: Tree maximum (int value) implementation attempt

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -14,5 +14,11 @@ object Tree {
     }
   }
 
+  def maximum(node: Tree[Int]): Int = {
+    node match {
+      case Leaf(v) => v
+      case Branch(l, r) => maximum(l).max(maximum(r))
+    }
+  }
 
 }
