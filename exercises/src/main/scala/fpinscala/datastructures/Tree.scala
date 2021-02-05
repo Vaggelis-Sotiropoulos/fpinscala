00df9cb0aa7a0a8dd1537c91a372062aa1588b5a
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-02-05 13:28:03 -0800

    FPSCALA-CHAPTER3: Edits Tree method formatting

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,18 +7,14 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
-  def size[A](node: Tree[A]): Int = {
-    node match {
+  def size[A](node: Tree[A]): Int = node match {
       case Leaf(_) => 1
       case Branch(l,r) => 1 + size(l) + size(r)
     }
-  }
 
-  def maximum(node: Tree[Int]): Int = {
-    node match {
+  def maximum(node: Tree[Int]): Int = node match {
       case Leaf(v) => v
       case Branch(l, r) => maximum(l).max(maximum(r))
     }
-  }
 
 }
