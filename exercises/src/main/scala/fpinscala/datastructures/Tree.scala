778b59fab248ed7d91a03f708843400d935349fa
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-02-05 14:03:01 -0800

    FPSCALA-CHAPTER3: Tree map implementation attempt

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -22,4 +22,8 @@ object Tree {
     case Branch(l,r) => 1 + maxPathLength(l).max(maxPathLength(r))
   }
 
+  def map[A, B](node: Tree[A])(f: A => B): Tree[B] = node match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 }
