f73530251835703cad0b2158a7b495587789fd91
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-02-05 13:15:52 -0800

    FPSCALA-CHAPTER3: Tree size implementation attempt

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,15 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
-
+  def size[A](node: Tree[A]): Int = {
+    def count(node1: Tree[A], count1: Int): Int = {
+      node1 match {
+        case Branch(None, None) => count1 + 1
+        case Branch(left, right) => count(left, count1) + count(right, count1)
+      }
+    }
+    count(node, 1)
+  }
 
 
 }
