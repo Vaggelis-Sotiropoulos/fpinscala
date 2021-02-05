be8b6a285c7918da4b5fc2a916205cf01921b158
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-02-05 13:44:46 -0800

    FPSCALA-CHAPTER3: Edits maxPathLength method

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -17,15 +17,9 @@ object Tree {
       case Branch(l, r) => maximum(l).max(maximum(r))
     }
 
-  def maxPathLength[A](node: Tree[A]): Int = {
-    def pathCount(node1: Tree[A], numPath: Int): Int ={
-      val numPath2 = numPath + 1
-      node1 match {
-        case Leaf(_) => numPath2
-        case Branch(l, r) => pathCount(l, numPath2).max(pathCount(r, numPath2))
-      }
-    }
-    pathCount(node, 0)
+  def maxPathLength[A](node: Tree[A]): Int = node match {
+    case Leaf(_) => 0
+    case Branch(l,r) => 1 + maxPathLength(l).max(maxPathLength(r))
   }
 
 }
