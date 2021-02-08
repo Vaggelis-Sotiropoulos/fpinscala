8038db3b92153a29994cc989f1fb1dd8a6d246f0
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-02-08 13:37:19 -0800

    FPSCALA-CHAPTER4: Option variance implementation attempt

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -5,27 +5,27 @@ import scala.{Option => _, Some => _, Either => _, _} // hide std library `Optio
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
-    case Some(a) => Some(f(a))
     case None => None
+    case Some(a) => Some(f(a))
   }
 
   def getOrElse[B>:A](default: => B): B = this match {
-    case Some(a) => a
     case None => default
+    case Some(a) => a
   }
 
   def flatMap[B](f: A => Option[B]): Option[B] = this match {
-    case Some(a) => f(a)
     case None => None
+    case Some(a) => f(a)
   }
 
   def orElse[B>:A](ob: => Option[B]): Option[B] = this match {
-    case Some(a) => this
     case None => ob
+    case Some(a) => this
   }
 
   def filter(f: A => Boolean): Option[A] = this match {
-    case Some(a) => if (f(a)) this else None
+    case Some(a) if f(a) => this
     case None => None
   }
 }
@@ -53,7 +53,9 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap(m => mean(xs.map(x => math.pow(x-m, 2))))
+  }
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
 
