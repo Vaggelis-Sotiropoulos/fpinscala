443d1fed9f41fcf79125721343044b18b26e6805
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-02-11 14:32:03 -0800

    FPSCALA-CHAPTER5: More unfold method implementation attempts

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -27,11 +27,11 @@ trait Stream[+A] {
     loop(this, List[A]())
   }
 
-  def take(n: Int): Stream[A] = this match {
-    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
-    case Cons(h, _) if n == 1 => cons(h(), empty)
-    case _ => empty
-  }
+  def take(n: Int): Stream[A] =
+    unfold(this){
+      case Cons(h, t) if n >= 1 => n-1; Some(h(), t())
+      case _ => None
+    }
 
   def drop(n: Int): Stream[A] = this match {
     case Cons(h, t) if n > 1 => t().drop(n - 1)
@@ -39,7 +39,10 @@ trait Stream[+A] {
     case _ => empty
   }
 
-  def takeWhile(p: A => Boolean): Stream[A] = foldRight(empty[A])((a,  b) => if (p(a)) cons(a, b) else empty)
+  def takeWhile(p: A => Boolean): Stream[A] = unfold(this){
+      case Cons(h, t) if p(h()) => Some(h(), t())
+      case _ => None
+    }
 
   def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
 
@@ -48,7 +51,23 @@ trait Stream[+A] {
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
-  def map[B](f: A => B): Stream[B] = foldRight(empty[B])((a, b) => cons(f(a), b))
+  def map[B](f: A => B): Stream[B] = unfold(this){
+      case Cons(h, t) => Some(f(h()), t())
+      case _ => None
+    }
+
+  def zipWith[B,C](s2: Stream[B])(f: (A,B) => C): Stream[C] = unfold((this, s2)){
+      case (empty, _) => None
+      case (_, empty) => None
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(f(h1(), h2()), (t1(), t2()))
+    }
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = unfold((this, s2)){
+    case (Cons(h1, t1), _) => Some((Some(h1()), None), (t1(), empty))
+    case (_, Cons(h2, t2)) => Some((None, Some(h2())), (empty, t2()))
+    case (Cons(h1, t1), Cons(h2, t2)) => Some((Some(h1()), Some(h2())), (t1(), t2()))
+    case (_, _) => None
+  }
 
   def filter(f: A => Boolean): Stream[A] = foldRight(empty[A])((a, b) => if (f(a)) cons(a, b) else b)
 
